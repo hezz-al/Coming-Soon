0eb90644c1f2a6dba492d54d9e07e6020b94bf7b
Orkhan <orkhandg@code.edu.az>
2020-03-01 15:10:52 +0400

    problem

--- Main Site/Models/Entity/Setting.cs
+++ Main Site/Models/Entity/Setting.cs
@@ -1,10 +1,15 @@
-﻿namespace MainSite.Models.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MainSite.Models.Entity
 {
     public class Setting:BaseEntity
     {
+        [Required]
         public string Slogan { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string PlayStore { get; set; }
         public string AppStore { get; set; }
